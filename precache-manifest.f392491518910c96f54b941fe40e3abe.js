694bcf2ab03f03f0ce7e7f5706132892d75515c4
Qifan Yang <yangqifan02@hotmail.com>
2019-04-12 14:51:53 +0200

    Updates

---
+++ precache-manifest.f392491518910c96f54b941fe40e3abe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "051f047e8a3eb5fa2c7e",
+    "url": "/resume/static/css/main.5c8eb4d4.chunk.css"
+  },
+  {
+    "revision": "051f047e8a3eb5fa2c7e",
+    "url": "/resume/static/js/main.997aa405.chunk.js"
+  },
+  {
+    "revision": "fd78f13f2eeb6b64bd90",
+    "url": "/resume/static/js/runtime~main.32a5f7b9.js"
+  },
+  {
+    "revision": "10534ad094e56cc863c6",
+    "url": "/resume/static/js/2.4b44abd2.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/resume/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b62ab1c1f5d22b9b6cbf44f520968be6",
+    "url": "/resume/index.html"
+  }
+];
