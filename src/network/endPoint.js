a877beb25491901b8243d1c8b08c927fc2f36feb
Qifan Yang <yangqifan02@hotmail.com>
2019-04-24 21:41:55 +0200

    add network fetch

--- src/network/endPoint.js
+++ src/network/endPoint.js
@@ -1,2 +1,2 @@
 export const endPoint =
-    'https://firebasestorage.googleapis.com/v0/b/viseo-mp-wallet.appspot.com/o/ABOUT.json?alt=media&token=f94752cc-02b9-4e35-bbb3-c10d9a273494';
+    'https://firebasestorage.googleapis.com/v0/b/resume-github-blog.appspot.com/o/data.json?alt=media&token=b276f39f-ad90-4b21-8502-18ac9ead0b87';
--- src/parser/parser.js
+++ src/parser/parser.js
@@ -21,3 +21,73 @@ export const parseExperiences = data => {
         }
     };
 };
+
+const parseEducation = data => ({
+    time: data.time,
+    collegeName: data.college,
+    title: data.title,
+    description: data.description
+});
+
+export const parseEducations = data => {
+    const { response, error } = data;
+    const educations =
+        response.layer.education && response.layer.education.length > 0
+            ? response.layer.education.map(experience =>
+                  parseEducation(response.content[experience])
+              )
+            : response.layer.education;
+
+    return {
+        error,
+        response: {
+            educations
+        }
+    };
+};
+
+const parseProject = data => ({
+    title: data.title,
+    clientIcon: data.client_ico,
+    clientLink: data.client_link,
+    description: data.description,
+    techstack: ['Kotlin, RxKotlin, Redux, Mapbox, Realm'],
+    projectLink: data.link
+});
+
+export const parseProjects = data => {
+    const { response, error } = data;
+    const projects =
+        response.layer.project && response.layer.project.length > 0
+            ? response.layer.project.map(experience =>
+                  parseProject(response.content[experience])
+              )
+            : response.layer.project;
+
+    return {
+        error,
+        response: {
+            projects
+        }
+    };
+};
+
+export const parseSkills = data => {};
+
+export const parseData = data => {
+    const { response, error } = data;
+    const { response: experiences } = parseExperiences(response);
+    const { response: educations } = parseEducations(response);
+    const { response: projects } = parseProjects(response);
+    const { response: skills } = parseSkills(response);
+
+    return {
+        error,
+        response: {
+            experiences,
+            educations,
+            projects,
+            skills
+        }
+    };
+};
