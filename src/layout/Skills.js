bf3f6d1908736c6db5ee9e7c618a1f4a153c393f
Qifan Yang <yangqifan02@hotmail.com>
2019-04-28 16:49:28 +0200

    add animation

--- src/layout/Skills.js
+++ src/layout/Skills.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Fade} from 'react-reveal';
 import {withStyles} from '@material-ui/core/styles';
 import SectionContainer from '../container/SectionContainer';
 import PageContainer from '../container/PageContainer';
@@ -9,14 +10,16 @@ import * as dimens from '../res/dimens';
 
 const Skills = ({ classes, skills, ...remainProps }) => {
     return (
-        <PageContainer className={classes.container}>
-            <SectionContainer
-                sectionIcon={strings.skills.icon}
-                sectionTitle={strings.skills.title}
-            >
-                <SkillInfo />
-            </SectionContainer>
-        </PageContainer>
+        <Fade>
+            <PageContainer className={classes.container}>
+                <SectionContainer
+                    sectionIcon={strings.skills.icon}
+                    sectionTitle={strings.skills.title}
+                >
+                    <SkillInfo />
+                </SectionContainer>
+            </PageContainer>
+        </Fade>
     );
 };
 const styles = {
