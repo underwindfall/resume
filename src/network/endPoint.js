34f6c46929373ed3541176d0361f23839d4e55fd
Qifan Yang <yangqifan02@hotmail.com>
2019-04-29 00:02:52 +0200

    remove useless imports

--- src/network/endPoint.js
+++ src/network/endPoint.js
@@ -1,2 +1,1 @@
-export const endPoint =
-    'https://firebasestorage.googleapis.com/v0/b/resume-github-blog.appspot.com/o/data.json?alt=media&token=cab8f6e1-0e8a-4d83-a361-fe96da8de079';
+export const endPoint = 'data.json';
