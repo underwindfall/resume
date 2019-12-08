38c20bfb3a36697c5cdd3f9cc00bb5b3a4a93663
Qifan Yang <yangqifan02@hotmail.com>
2019-12-08 22:47:00 +0100

    Updates

---
+++ precache-manifest.1bcf2d7b9bd9ff904bf3766b41ed0a0e.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dece66abd74f1c331f8f95f3edf3aaf4",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "30bb27104ed5ce25edaf",
+    "url": "/resume/static/css/main.b4678765.chunk.css"
+  },
+  {
+    "revision": "319fa7a791c72f2f3665",
+    "url": "/resume/static/js/2.47fb6623.chunk.js"
+  },
+  {
+    "revision": "12b359b26bf963324cf29f1846cb7669",
+    "url": "/resume/static/js/2.47fb6623.chunk.js.LICENSE"
+  },
+  {
+    "revision": "30bb27104ed5ce25edaf",
+    "url": "/resume/static/js/main.47d0e640.chunk.js"
+  },
+  {
+    "revision": "754cb4722296976aff30",
+    "url": "/resume/static/js/runtime-main.bef2460a.js"
+  },
+  {
+    "revision": "d3719fc1c128e1a973a5a5e6d502de5f",
+    "url": "/resume/static/media/avatar.d3719fc1.jpg"
+  },
+  {
+    "revision": "03a485c070109709a5e1f7fc1ce0368d",
+    "url": "/resume/static/media/banner.03a485c0.jpg"
+  },
+  {
+    "revision": "0dd9dfa98f23fbb50a3adeec8b783a3b",
+    "url": "/resume/static/media/bel.0dd9dfa9.png"
+  },
+  {
+    "revision": "5ad016b9ade4ee41c39b5c12208d7199",
+    "url": "/resume/static/media/carrefour.5ad016b9.png"
+  },
+  {
+    "revision": "bc970b5c73f6f6de3da2cc4185b9c817",
+    "url": "/resume/static/media/css.bc970b5c.png"
+  },
+  {
+    "revision": "3e4c258129331e604bf9c8bf77dd270a",
+    "url": "/resume/static/media/czam.3e4c2581.png"
+  },
+  {
+    "revision": "f18eddfeb1824f2cfaf78632a3a3d7ff",
+    "url": "/resume/static/media/java.f18eddfe.png"
+  },
+  {
+    "revision": "5744ba58167c3052307b0da70a031911",
+    "url": "/resume/static/media/javascript.5744ba58.png"
+  },
+  {
+    "revision": "ebad43b970b152a6fb343f642b7d79af",
+    "url": "/resume/static/media/jekyll.ebad43b9.png"
+  },
+  {
+    "revision": "489912a1bf60a9513ab32492a90763e2",
+    "url": "/resume/static/media/jenkins.489912a1.png"
+  },
+  {
+    "revision": "bd5b7b39869b8d508dc4840ad94a7dc2",
+    "url": "/resume/static/media/kotlin.bd5b7b39.png"
+  },
+  {
+    "revision": "d5cc2d8b2bbd43cffd82cd2464e578a7",
+    "url": "/resume/static/media/rn.d5cc2d8b.jpeg"
+  },
+  {
+    "revision": "f0289b01b57428cb2e4f000de26b68c2",
+    "url": "/resume/static/media/ruby.f0289b01.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/resume/precache-manifest.a7c89fac3313e5b05c3b61a432d3ec4a.js"
+  "/resume/precache-manifest.1bcf2d7b9bd9ff904bf3766b41ed0a0e.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/resume/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/resume/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
