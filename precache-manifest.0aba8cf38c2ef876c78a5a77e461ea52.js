c9f875abb7aa73693bc2ffae3bc60af57d3e9b97
underwindfall <yangqifan02@hotmail.com>
2019-12-09 10:20:45 +0000

    Deploying to gh-pages from master 63727a22ffbddab22ca7e10c830de6342d5950d5

---
+++ precache-manifest.0aba8cf38c2ef876c78a5a77e461ea52.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc970b5c73f6f6de3da2cc4185b9c817",
+    "url": "/resume/static/media/css.bc970b5c.png"
+  },
+  {
+    "revision": "68b2267b4983ad690f97",
+    "url": "/resume/static/css/main.16b7e4b2.chunk.css"
+  },
+  {
+    "revision": "fd78f13f2eeb6b64bd90",
+    "url": "/resume/static/js/runtime~main.32a5f7b9.js"
+  },
+  {
+    "revision": "78a950f03ccde7b98628",
+    "url": "/resume/static/js/2.e4aae7f8.chunk.js"
+  },
+  {
+    "revision": "03a485c070109709a5e1f7fc1ce0368d",
+    "url": "/resume/static/media/banner.03a485c0.jpg"
+  },
+  {
+    "revision": "d3719fc1c128e1a973a5a5e6d502de5f",
+    "url": "/resume/static/media/avatar.d3719fc1.jpg"
+  },
+  {
+    "revision": "bd5b7b39869b8d508dc4840ad94a7dc2",
+    "url": "/resume/static/media/kotlin.bd5b7b39.png"
+  },
+  {
+    "revision": "f18eddfeb1824f2cfaf78632a3a3d7ff",
+    "url": "/resume/static/media/java.f18eddfe.png"
+  },
+  {
+    "revision": "d5cc2d8b2bbd43cffd82cd2464e578a7",
+    "url": "/resume/static/media/rn.d5cc2d8b.jpeg"
+  },
+  {
+    "revision": "68b2267b4983ad690f97",
+    "url": "/resume/static/js/main.54f9bfdb.chunk.js"
+  },
+  {
+    "revision": "5744ba58167c3052307b0da70a031911",
+    "url": "/resume/static/media/javascript.5744ba58.png"
+  },
+  {
+    "revision": "ebad43b970b152a6fb343f642b7d79af",
+    "url": "/resume/static/media/jekyll.ebad43b9.png"
+  },
+  {
+    "revision": "f0289b01b57428cb2e4f000de26b68c2",
+    "url": "/resume/static/media/ruby.f0289b01.png"
+  },
+  {
+    "revision": "489912a1bf60a9513ab32492a90763e2",
+    "url": "/resume/static/media/jenkins.489912a1.png"
+  },
+  {
+    "revision": "5ad016b9ade4ee41c39b5c12208d7199",
+    "url": "/resume/static/media/carrefour.5ad016b9.png"
+  },
+  {
+    "revision": "0dd9dfa98f23fbb50a3adeec8b783a3b",
+    "url": "/resume/static/media/bel.0dd9dfa9.png"
+  },
+  {
+    "revision": "3e4c258129331e604bf9c8bf77dd270a",
+    "url": "/resume/static/media/czam.3e4c2581.png"
+  },
+  {
+    "revision": "f567a18045511a0c3f918259da33a547",
+    "url": "/resume/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,19 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/resume/precache-manifest.1bcf2d7b9bd9ff904bf3766b41ed0a0e.js"
+  "/resume/precache-manifest.0aba8cf38c2ef876c78a5a77e461ea52.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
+workbox.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -31,9 +25,10 @@ workbox.core.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/resume/index.html"), {
+workbox.routing.registerNavigationRoute("/resume/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
