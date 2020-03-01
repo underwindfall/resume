ba008063fd8fc6dca73a509f4ce2d25334961006
underwindfall <yangqifan02@hotmail.com>
2020-03-01 15:53:24 +0000

    Deploying to gh-pages from master - 2ea6ee9a2bbca7b75b54c9dcb85a9d615e6aa0a2 🚀

---
+++ precache-manifest.7fd5b328a8dd32b8bb52eb69c2b3b91c.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11ea322d1daf9af33b4d724044c5bd3a",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "d88d46474ea1750bf3ba",
+    "url": "/resume/static/css/main.85432186.chunk.css"
+  },
+  {
+    "revision": "343c5d5c9e54684cc352",
+    "url": "/resume/static/js/2.d0ec1774.chunk.js"
+  },
+  {
+    "revision": "12b359b26bf963324cf29f1846cb7669",
+    "url": "/resume/static/js/2.d0ec1774.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bbb18290abf1fe273f84",
+    "url": "/resume/static/js/3.d2e2ae81.chunk.js"
+  },
+  {
+    "revision": "d88d46474ea1750bf3ba",
+    "url": "/resume/static/js/main.69152256.chunk.js"
+  },
+  {
+    "revision": "efaf2464a56b3a37a1ad",
+    "url": "/resume/static/js/runtime-main.6fe41193.js"
+  },
+  {
+    "revision": "d3719fc1c128e1a973a5a5e6d502de5f",
+    "url": "/resume/static/media/avatar.d3719fc1.jpg"
+  },
+  {
+    "revision": "03a485c070109709a5e1f7fc1ce0368d",
+    "url": "/resume/static/media/banner.03a485c0.jpg"
+  },
+  {
+    "revision": "0dd9dfa98f23fbb50a3adeec8b783a3b",
+    "url": "/resume/static/media/bel.0dd9dfa9.png"
+  },
+  {
+    "revision": "5ad016b9ade4ee41c39b5c12208d7199",
+    "url": "/resume/static/media/carrefour.5ad016b9.png"
+  },
+  {
+    "revision": "bc970b5c73f6f6de3da2cc4185b9c817",
+    "url": "/resume/static/media/css.bc970b5c.png"
+  },
+  {
+    "revision": "3e4c258129331e604bf9c8bf77dd270a",
+    "url": "/resume/static/media/czam.3e4c2581.png"
+  },
+  {
+    "revision": "f18eddfeb1824f2cfaf78632a3a3d7ff",
+    "url": "/resume/static/media/java.f18eddfe.png"
+  },
+  {
+    "revision": "5744ba58167c3052307b0da70a031911",
+    "url": "/resume/static/media/javascript.5744ba58.png"
+  },
+  {
+    "revision": "ebad43b970b152a6fb343f642b7d79af",
+    "url": "/resume/static/media/jekyll.ebad43b9.png"
+  },
+  {
+    "revision": "489912a1bf60a9513ab32492a90763e2",
+    "url": "/resume/static/media/jenkins.489912a1.png"
+  },
+  {
+    "revision": "bd5b7b39869b8d508dc4840ad94a7dc2",
+    "url": "/resume/static/media/kotlin.bd5b7b39.png"
+  },
+  {
+    "revision": "828b317c24ed05b1ac143973fa40b592",
+    "url": "/resume/static/media/netatmo.828b317c.jpg"
+  },
+  {
+    "revision": "d5cc2d8b2bbd43cffd82cd2464e578a7",
+    "url": "/resume/static/media/rn.d5cc2d8b.jpeg"
+  },
+  {
+    "revision": "f0289b01b57428cb2e4f000de26b68c2",
+    "url": "/resume/static/media/ruby.f0289b01.png"
+  }
+]);
